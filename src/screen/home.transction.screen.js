d3f5142a5955f2a0dfc8480e99e657cce88d8376
owen.bringino <owen.bringino@metrobank.com.ph>
2022-07-10 09:34:22 +0800

    transaction: add headline

--- src/screen/home.transction.screen.js
+++ src/screen/home.transction.screen.js
@@ -8,10 +8,40 @@ import { Chip } from 'react-native-paper'
 const DATE_FORMAT = 'YYYY-MM-DD'
 const DATE_FORMAT_SHORT = 'DD (ddd)'
 const DATE_DIVIDER = '-date-divider-'
+const PESO_SYMBOL = '\u20B1'
 
-function currencyFormat(numStr) {
+function currencyFormat(numStr, symbol) {
   const num = parseFloat(numStr)
-  return num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  sym = symbol || ''
+  return sym + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+const Headline = ({ income, expense }) => {
+  const savings = income - expense
+  const expenseStyle = savings < 0.0 ? { color: 'red' } : {}
+  const savingsAbs =
+    savings < 0.0
+      ? '(' + currencyFormat(savings * -1, PESO_SYMBOL) + ')'
+      : currencyFormat(savings, PESO_SYMBOL)
+
+  return (
+    <View style={styles.headline}>
+      <View style={{ ...styles.headlineColumn, backgroundColor: 'aquamarine' }}>
+        <Text style={{ color: 'darkgreen' }}>Income</Text>
+        <Text style={styles.amount}>{currencyFormat(income, PESO_SYMBOL)}</Text>
+      </View>
+      <View style={{ ...styles.headlineColumn, backgroundColor: 'lightpink' }}>
+        <Text style={{ color: 'purple' }}>Expense</Text>
+        <Text style={styles.amount}>
+          {currencyFormat(expense, PESO_SYMBOL)}
+        </Text>
+      </View>
+      <View style={styles.headlineColumn}>
+        <Text style={{ color: 'blue' }}>Savings</Text>
+        <Text style={{ ...styles.amount, ...expenseStyle }}>{savingsAbs}</Text>
+      </View>
+    </View>
+  )
 }
 
 const TransactionScreen = txnScreen => {
@@ -19,8 +49,13 @@ const TransactionScreen = txnScreen => {
   const journal = useSelector(state => state.journal)
 
   const [entries, setEntries] = useState([])
+  const [income, setIncome] = useState(0.0)
+  const [expense, setExpense] = useState(0.0)
 
   useEffect(() => {
+    let inc = 0.0
+    let exp = 0.0
+
     const arr = []
       .concat(journal.entries)
       // sort by date
@@ -29,18 +64,24 @@ const TransactionScreen = txnScreen => {
         d2 = moment(b.date, DATE_FORMAT)
         return d1.isAfter(d2)
       })
-      // add date separator
+      // process data
       .flatMap((item, idx, list) => {
-        d1 = (
+        const { amount, income } = item
+        const a = parseFloat(amount)
+
+        // compute summary
+        if (income === 'false') exp += a
+        else inc += a
+
+        const d1 = (
           idx === 0 ? moment() : moment(list[idx - 1].date, DATE_FORMAT)
         ).format(DATE_FORMAT_SHORT)
-        d2 = moment(item.date, DATE_FORMAT).format(DATE_FORMAT_SHORT)
-
-        console.log({ idx, d1, d2 })
+        const d2 = moment(item.date, DATE_FORMAT).format(DATE_FORMAT_SHORT)
 
         return d1 === d2
           ? [item]
           : [
+              // add date separator
               {
                 category: DATE_DIVIDER,
                 date: d2,
@@ -48,6 +89,9 @@ const TransactionScreen = txnScreen => {
               item,
             ]
       })
+
+    setIncome(inc)
+    setExpense(exp)
 
     setEntries(arr)
   }, [journal])
@@ -57,7 +101,7 @@ const TransactionScreen = txnScreen => {
 
     if (category === DATE_DIVIDER) {
       return (
-        <View>
+        <View style={styles.dateDivider}>
           <Text>{date}</Text>
         </View>
       )
@@ -90,26 +134,30 @@ const TransactionScreen = txnScreen => {
 
   return (
     <View>
-      <Text>Here's your transaction</Text>
+      <Headline expense={expense} income={income} />
       <FlatList data={entries} renderItem={renderItem} />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  headline: {
+    flexDirection: 'row',
+  },
+  headlineColumn: {
+    width: '32%',
+    backgroundColor: 'skyblue',
+    borderRadius: 5,
+    padding: 2,
+    margin: 2,
+  },
   contentContainer: {
     flex: 1,
     flexDirection: 'row',
-    //backgroundColor: 'skyblue',
-    width: '100%',
-  },
-  contentContainerColumn1: {
-    flex: 1,
-    width: '50%',
-  },
-  contentContainerColumn2: {
-    flex: 2,
-    backgroundColor: 'pink',
+    marginLeft: 10,
+    marginRight: 5,
+    marginTop: 1,
+    marginBottom: 1,
   },
   amountColumn: {
     flex: 1,
@@ -123,6 +171,12 @@ const styles = StyleSheet.create({
   },
   amount: {
     fontSize: 18,
+  },
+  dateDivider: {
+    backgroundColor: 'lightgray',
+    padding: 3,
+    marginTop: 3,
+    marginBottom: 1,
   },
 })
 
